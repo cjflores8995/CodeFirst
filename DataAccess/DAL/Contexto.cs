a83e2f29551fd8515bd9fab7a7b63668781c5b4c
= <carlos_jhousef593@hotmail.com>
2020-09-12 19:49:21 -0500

    first commit

---
+++ DataAccess/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAL
+{
+    public class Contexto: DbContext
+    {
+        public Contexto() : base("MiBaseDeDatos") {
+
+            Database.SetInitializer<Contexto>(new DropCreateDatabaseIfModelChanges<Contexto>());
+        }
+
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
